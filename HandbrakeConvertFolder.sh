bb1a724148bd3e90b513083ecd683b25c5bf7961
David Brunnthaler <david@screencode.at>
2018-02-11 16:27:29 +0100

    HandbrakeConvertFolder: userInput Folder escape special characters

--- HandbrakeConvertFolder.sh
+++ HandbrakeConvertFolder.sh
@@ -21,29 +21,32 @@ if [ "${userSpecifiedFolder: -1}" = "/" ]; then
 fi
 
 # create "converted" folder
-finalFolder="${userSpecifiedFolder}_converted"
-mkdir -p "${finalFolder}"
+destinationFolder="${userSpecifiedFolder}_converted"
+mkdir -p "${destinationFolder}"
+
+# escape special characters
+printf -v userSpecifiedFolderEsc "%q" "$userSpecifiedFolder"
+printf -v destinationFolderEsc "%q" "$destinationFolder"
 
 # go through files in user specified folder and convert them
 for entry in "$userSpecifiedFolder"/*
 do
     if [ -f "$entry" ]; then
         export fspec=$entry
-        fdir=$(dirname "$fspec")
         fnameWithExt=$(basename "$fspec")
-        fnameWitoutExt="${fnameWithExt%.*}"
+        fnameWithoutExt="${fnameWithExt%.*}"
         fext="${fnameWithExt##*.}"
 
         #echo $fspec
         #echo $fdir
         #echo $fnameWithExt
-        #echo $fnameWitoutExt
+        #echo $fnameWithoutExt
         #echo $fext
 
         if [ "$finalCommand" != "" ]; then
             finalCommand="$finalCommand; "
         fi
-        finalCommand="$finalCommand HandBrakeCLI -i $fspec -o $finalFolder/$fnameWitoutExt.mp4"
+        finalCommand="$finalCommand HandBrakeCLI -i $userSpecifiedFolderEsc/$fnameWithExt -o $destinationFolderEsc/$fnameWithoutExt.mp4"
     fi
 done
 
